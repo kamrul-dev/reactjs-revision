8988a7444f406eeb7504241303f6a83945611915
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-18 19:04:28 +0600

    style added in heading of blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
+const headingStyle ={
+  color: 'green',
+  backgroundColor: 'lightblue',
+  display: 'inline',
+  padding: '5px',
+  borderRadius: '10px'
+
+}
+
 function App() {
   return (
     <div className="App">
@@ -8,6 +19,7 @@ function App() {
       {
         <div>
           <article className='blog'>
+            <h2 style={headingStyle}>Blog post 1</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio, corrupti aperiam exercitationem quisquam voluptate labore qui perferendis in error sit fugiat quas iusto minima vero, ipsa quam cupiditate beatae.
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio, corrupti aperiam exercitationem quisquam voluptate labore qui perferendis in error sit fugiat quas iusto minima vero, ipsa quam cupiditate beatae.</p>
           </article>

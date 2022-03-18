877e825c1174a130184ef8efdea0e0ddbb6d365c
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-18 18:36:46 +0600

    article tag with blog post and blog class style added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>This is Blog post</h1>
+      {
+        <div>
+          <article className='blog'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio, corrupti aperiam exercitationem quisquam voluptate labore qui perferendis in error sit fugiat quas iusto minima vero, ipsa quam cupiditate beatae.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio, corrupti aperiam exercitationem quisquam voluptate labore qui perferendis in error sit fugiat quas iusto minima vero, ipsa quam cupiditate beatae.</p>
+          </article>
+        </div>
+      }
     </div>
   );
 }

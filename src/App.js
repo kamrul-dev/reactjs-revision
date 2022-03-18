98406bb86848479d72c6e561fc82110b6ded0bd8
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-18 19:35:22 +0600

    Blog post updated

--- src/App.js
+++ src/App.js
@@ -14,15 +14,16 @@ const headingStyle = {
 
 function App() {
   return (
-    
+
     <div className="App">
       <h1>This is Blog post</h1>
       {
         <div>
           <article className='blog'>
-            <h2 style={headingStyle}>Blog post 1</h2>
-            <p style={{ fontSize: '20px', color: 'green', textAlign: 'justify' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio, corrupti aperiam exercitationem quisquam voluptate labore qui perferendis in error sit fugiat quas iusto minima vero, ipsa quam cupiditate beatae.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis odio, corrupti aperiam exercitationem quisquam voluptate labore qui perferendis in error sit fugiat quas iusto minima vero, ipsa quam cupiditate beatae.</p>
+            <h2 style={headingStyle}>How does javascript  work?</h2>
+            <p style={{ fontSize: '20px', color: 'green', textAlign: 'justify' }}>
+              We know that javascript code runs on the browser. The browser has a browser engine to execute the program. Google chrome has v8 engine, fire-fox has spider monkey engine, internet explorer has chakra engine. Let's take an example v8 engine of chrome browser. When v8 engine takes the javascript code from the js program source files then v8 engine starts parsing the code using “parser”. Then parsing is converted into “Abstract Syntax Tree(AST). After that interpreter ignition converts this Abstract Syntax Tree to “Bytecode” or by using “Just in Time Compiler “optimized machine code”. Just in Time Compiler Increases the performance and code runs faster. In this way javascript code works in v8 engine to execute the program.
+            </p>
           </article>
         </div>
       }
